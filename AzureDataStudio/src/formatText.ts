71e3f2e3a26cf192a924c56abba17e4e63978b81
Brett Huber <bretthuber6@gmail.com>
2021-10-20 22:14:38 -0500

    check if all text is numeric

--- AzureDataStudio/src/formatText.ts
+++ AzureDataStudio/src/formatText.ts
@@ -1,5 +1,11 @@
 
 
 export function formatText(text: string, formatAsString: boolean) {
+    const splitText = text.split(/\r?\n/);
 
+    const isTextAllNumeric = formatAsString ? false : isAllTextNumeric(splitText);
+}
+
+function isAllTextNumeric(text: string[]): boolean {
+    return text.every(t => typeof t === 'number');
 }
