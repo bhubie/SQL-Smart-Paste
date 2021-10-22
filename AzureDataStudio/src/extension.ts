4a8c228522af0d96234b1123a66409d17511b7ce
Brett Huber <bretthuber6@gmail.com>
2021-10-21 22:28:31 -0500

    first pass for logic of grabbin text, formatting, and pasting to editor

--- AzureDataStudio/src/extension.ts
+++ AzureDataStudio/src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { formatText } from './formatText';
 
 // The module 'azdata' contains the Azure Data Studio extensibility API
 // This is a complementary set of APIs that add SQL / Data-specific functionality to the app
@@ -11,25 +12,31 @@ import * as azdata from 'azdata';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+
+async function formatClipboardTextAndPasteToEditor(formatAsString: boolean) {
+    const clipboardContent = await vscode.env.clipboard.readText();
+    const formattedText = formatText(clipboardContent, formatAsString);
+    
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+        editor.edit(editBuilder => {
+            editBuilder.insert(editor.selection.active, formattedText);
+        });
+    }
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
    
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    context.subscriptions.push(vscode.commands.registerCommand('sql-smart-paste.pasteascsv', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    context.subscriptions.push(vscode.commands.registerCommand('sql-smart-paste.pasteascsv', async () => {
+        await formatClipboardTextAndPasteToEditor(false);
     }));
 
     context.subscriptions.push(vscode.commands.registerCommand('sql-smart-paste.pasteasvarcharcsv', async () => {
-        // The code you place here will be executed every time your command is executed
-
-        let clipboardContent = await vscode.env.clipboard.readText(); 
-        // Display a message box to the user
-        vscode.window.showInformationMessage(clipboardContent);
+        await formatClipboardTextAndPasteToEditor(true);
     }));
 }
 
--- AzureDataStudio/src/formatText.ts
+++ AzureDataStudio/src/formatText.ts
@@ -1,11 +1,38 @@
-
-
 export function formatText(text: string, formatAsString: boolean) {
-    const splitText = text.split(/\r?\n/);
+    const splitText = text.split(/\r?\n/).filter(x => x !== '');
 
     const isTextAllNumeric = formatAsString ? false : isAllTextNumeric(splitText);
+
+    return joinText(splitText, isTextAllNumeric);
 }
 
 function isAllTextNumeric(text: string[]): boolean {
-    return text.every(t => typeof t === 'number');
+    return text.every(t => parseInt(t));
+}
+
+function joinText(text: string[], isTextAllNumeric: boolean): string {
+    
+    let joinedText: string = '';
+
+    text.forEach((el, index, arr) => {
+        // Properly Escape apostrophes
+        let t = el.replace("'", "''");
+
+        if (isTextAllNumeric)
+        {
+            joinedText += t;
+        }
+        else
+        {
+            joinedText += `'${t}'`;
+        }
+
+        if(!(index === arr.length - 1))
+        {
+            joinedText += ",";
+            joinedText += "\n";
+        }
+    });
+
+    return joinedText;
 }
